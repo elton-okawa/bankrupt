17b9f63e3fd2090e16521f86ba0631e0fa983460
elton-okawa <elton_okawa@hotmail.com>
2018-04-15 07:46:05 -0300

    working fine

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main (String args[]) throws IOException {
         GameControl gameControl = new GameControl(GameConstants.GAME_CONFIG_FILE_NAME);
 
-//        for (int i = 0; i < GameConstants.NUMBER_OF_GAMES; i++) {
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < GameConstants.NUMBER_OF_GAMES; i++) {
+//        for (int i = 0; i < 2; i++){
             gameControl.startGame();
             gameControl.init(GameConstants.GAME_CONFIG_FILE_NAME);
         }
